7a299482c464bdb0b89b76281cab00038f9f1a58
danthe <danthe666@gmail.com>
2020-12-17 23:23:30 +0100

    uwebsockets Windows port ; Jerk Minimizing & Polynomial Trajectory Generators added

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,17 @@
-#include <uWS/uWS.h>
+
+// projs:
+// GNB - Naive Bayesian
+// BehaviorPlanner
+// hibridastar
+// PTG - Polynomial Trajectory Generator
+
+
+#ifdef UWS_VCPKG
+    #include <uwebsockets/App.h>
+#else
+    #include <uWS/uWS.h>
+#endif 
+
 #include <fstream>
 #include <iostream>
 #include <string>
@@ -13,6 +26,7 @@ using nlohmann::json;
 using std::string;
 using std::vector;
 
+#ifndef UWS_VCPKG
 int main() {
   uWS::Hub h;
 
@@ -134,3 +148,133 @@ int main() {
   
   h.run();
 }
+
+#else 
+
+int main() {
+
+    // Load up map values for waypoint's x,y,s and d normalized normal vectors
+    vector<double> map_waypoints_x;
+    vector<double> map_waypoints_y;
+    vector<double> map_waypoints_s;
+    vector<double> map_waypoints_dx;
+    vector<double> map_waypoints_dy;
+
+    // Waypoint map to read from
+    string map_file_ = "../data/highway_map.csv";
+    // The max s value before wrapping around the track back to 0
+    double max_s = 6945.554;
+
+    std::ifstream in_map_(map_file_.c_str(), std::ifstream::in);
+
+    string line;
+    while (getline(in_map_, line)) {
+        std::istringstream iss(line);
+        double x;
+        double y;
+        float s;
+        float d_x;
+        float d_y;
+        iss >> x;
+        iss >> y;
+        iss >> s;
+        iss >> d_x;
+        iss >> d_y;
+        map_waypoints_x.push_back(x);
+        map_waypoints_y.push_back(y);
+        map_waypoints_s.push_back(s);
+        map_waypoints_dx.push_back(d_x);
+        map_waypoints_dy.push_back(d_y);
+    }
+	
+    struct PerSocketData {
+
+	};
+
+    int port = 4567;
+
+    uWS::App::WebSocketBehavior b;
+    b.open = [](auto* ws) {
+        std::cout << "Connected!!!" << std::endl;
+    };
+    b.close = [](auto* ws, int /*code*/, std::string_view /*message*/) {
+        ws->close();
+        std::cout << "Disconnected" << std::endl;
+    };
+	b.message = [&map_waypoints_x, &map_waypoints_y, &map_waypoints_s,
+		&map_waypoints_dx, &map_waypoints_dy] (auto* ws, std::string_view message, uWS::OpCode opCode) {
+        // "42" at the start of the message means there's a websocket message event.
+        // The 4 signifies a websocket message
+        // The 2 signifies a websocket event
+        size_t length = message.length();
+        const char* data = message.data();
+        if (length && length > 2 && data[0] == '4' && data[1] == '2') {
+
+            auto s = hasData(data);
+
+            if (s != "") {
+                auto j = json::parse(s);
+
+                string event = j[0].get<string>();
+
+                if (event == "telemetry") {
+                    // j[1] is the data JSON object
+
+                    // Main car's localization Data
+                    double car_x = j[1]["x"];
+                    double car_y = j[1]["y"];
+                    double car_s = j[1]["s"];
+                    double car_d = j[1]["d"];
+                    double car_yaw = j[1]["yaw"];
+                    double car_speed = j[1]["speed"];
+
+                    // Previous path data given to the Planner
+                    auto previous_path_x = j[1]["previous_path_x"];
+                    auto previous_path_y = j[1]["previous_path_y"];
+                    // Previous path's end s and d values 
+                    double end_path_s = j[1]["end_path_s"];
+                    double end_path_d = j[1]["end_path_d"];
+
+                    // Sensor Fusion Data, a list of all other cars on the same side 
+                    //   of the road.
+                    auto sensor_fusion = j[1]["sensor_fusion"];
+
+                    json msgJson;
+
+                    vector<double> next_x_vals;
+                    vector<double> next_y_vals;
+
+                    /**
+                     * TODO: define a path made up of (x,y) points that the car will visit
+                     *   sequentially every .02 seconds
+                     */
+
+
+                    msgJson["next_x"] = next_x_vals;
+                    msgJson["next_y"] = next_y_vals;
+
+                    auto msg = "42[\"control\"," + msgJson.dump() + "]";
+
+                    ws->send(msg, uWS::OpCode::TEXT);
+                }  // end "telemetry" if
+            }
+            else {
+                // Manual driving
+                std::string msg = "42[\"manual\",{}]";
+                ws->send(msg, uWS::OpCode::TEXT);
+            }
+        }  // end websocket if
+    };
+
+    uWS::App().ws<PerSocketData>("/*", std::move(b)).listen("127.0.0.1", port, [port](auto* listen_socket) {
+        if (listen_socket) {
+            std::cout << "Listening on port " << port << std::endl;
+        }
+        else {
+			std::cerr << "Failed to listen to port" << std::endl;
+			exit(-1);
+        }
+    }).run();
+}
+
+#endif 
